c75c172429ff9a6048ddac64da4b6796b53dd78a
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-02-06 22:05:34 +0900

    Update tabbar image size

--- app/src/main/java/com/example/bbip_clone/ui/TabBar.kt
+++ app/src/main/java/com/example/bbip_clone/ui/TabBar.kt
@@ -5,8 +5,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -24,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,7 +31,6 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.bbip_clone.R
 
-@Suppress("UNUSED_EXPRESSION")
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter", "AutoboxingStateValueProperty")
 @Composable
 fun TabScreen(navController: NavController) {
@@ -44,9 +43,8 @@ fun TabScreen(navController: NavController) {
                 Image(
                     painter = painterResource(id = R.drawable.tab_bar),
                     contentDescription = null,
-                    modifier = Modifier
-                        .fillMaxHeight(0.15f)
-                        .fillMaxSize()
+                    modifier = Modifier.fillMaxWidth(),
+                    contentScale = ContentScale.Crop
                 )
                 TabRow(
                     selectedTabIndex = pagerState.value,
@@ -62,8 +60,8 @@ fun TabScreen(navController: NavController) {
                             icon = {
                                 Icon(
                                     modifier = Modifier
-                                        .size(45.dp)
-                                        .padding(bottom = 10.dp)
+                                        .size(40.dp)
+                                        .padding(bottom = 5.dp)
                                         .background(Color.White),
                                     imageVector = icon,
                                     contentDescription = null,
