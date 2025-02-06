1b9e6607aebe564e88d2852db8d6a9b51e33019d
Taehoon Kim <124406982+taeh-kim@users.noreply.github.com>
2025-02-06 22:06:54 +0900

    add Font(based on PR review - TextStyple)

--- app/src/main/java/com/example/bbip_clone/ui/theme/Type.kt
+++ app/src/main/java/com/example/bbip_clone/ui/theme/Type.kt
@@ -9,20 +9,20 @@ import androidx.compose.ui.unit.sp
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = wantedSans,
+        fontWeight = FontWeight.W500,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = wantedSans,
+        fontWeight = FontWeight.W700,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
+    /*
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
--- app/src/main/java/com/example/bbip_clone/ui/theme/font.kt
+++ app/src/main/java/com/example/bbip_clone/ui/theme/font.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import com.example.bbip_clone.R
 
-val wantedsans = FontFamily(
+val wantedSans = FontFamily(
     Font(R.font.wantedsans_medium, FontWeight.W500),
     Font(R.font.wantedsans_bold, FontWeight.W700)
 )
