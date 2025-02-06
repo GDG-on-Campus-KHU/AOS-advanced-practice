5b7562df8d6c41fe22e473a863cc93c50b24c05d
Taehoon Kim <124406982+taeh-kim@users.noreply.github.com>
2025-02-06 09:59:14 +0900

    add Font

---
+++ app/src/main/java/com/example/bbip_clone/ui/theme/font.kt
@@ -0,0 +1,11 @@
+package com.example.bbip_clone.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.bbip_clone.R
+
+val wantedsans = FontFamily(
+    Font(R.font.wantedsans_medium, FontWeight.W500),
+    Font(R.font.wantedsans_bold, FontWeight.W700)
+)
