f18b4af7bd74a381214f7500c5ce67867b4ca151
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-02-07 00:17:13 +0900

    modify tab size & disable ripple effect

--- app/src/main/java/com/example/bbip_clone/ui/TabBar.kt
+++ app/src/main/java/com/example/bbip_clone/ui/TabBar.kt
@@ -3,13 +3,13 @@ package com.example.bbip_clone.ui
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
@@ -18,8 +18,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,11 +33,11 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.bbip_clone.R
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter", "AutoboxingStateValueProperty")
+@SuppressLint("UnrememberedMutableInteractionSource", "UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun TabScreen(navController: NavController) {
     val tabs = listOf(Icons.Filled.Home, Icons.Filled.DateRange)
-    val pagerState = remember { mutableIntStateOf(0) }
+    var pagerState by remember { mutableIntStateOf(0) }
 
     Scaffold(
         bottomBar = {
@@ -43,41 +45,47 @@ fun TabScreen(navController: NavController) {
                 Image(
                     painter = painterResource(id = R.drawable.tab_bar),
                     contentDescription = null,
-                    modifier = Modifier.fillMaxWidth(),
-                    contentScale = ContentScale.Crop
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(127.dp),
+                    contentScale = ContentScale.FillBounds
                 )
                 TabRow(
-                    selectedTabIndex = pagerState.value,
+                    selectedTabIndex = pagerState,
                     containerColor = Color.Transparent,
                     indicator = {},
                     divider = {}
                 ) {
                     tabs.forEachIndexed { index, icon ->
                         Tab(
-                            modifier = Modifier.height(90.dp),
-                            selected = pagerState.value == index,
-                            onClick = { pagerState.value = index },
+                            modifier = Modifier.height(100.dp),
+                            selected = pagerState == index,
+                            onClick = { },
+                            enabled = false,
                             icon = {
                                 Icon(
                                     modifier = Modifier
-                                        .size(40.dp)
-                                        .padding(bottom = 5.dp)
-                                        .background(Color.White),
+                                        .size(30.dp)
+                                        .background(Color.White)
+                                        .clickable(
+                                            interactionSource = MutableInteractionSource(),
+                                            indication = null
+                                        ) { pagerState = index },
                                     imageVector = icon,
                                     contentDescription = null,
-                                    tint = if (pagerState.value == index) Color.Black else Color.LightGray
+                                    tint = if (pagerState == index) Color.Black else Color.LightGray
                                 )
                             }
                         )
                         if (index < tabs.size - 1) {
-                            Spacer(modifier = Modifier.width(16.dp))
+                            Spacer(modifier = Modifier.fillMaxWidth())
                         }
                     }
                 }
             }
         }
     ) {
-        when (pagerState.value) {
+        when (pagerState) {
             0 -> UserHomeScreen(navController)
             1 -> StudyHomeScreen(navController)
         }
