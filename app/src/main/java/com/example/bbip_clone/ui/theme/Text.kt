5396c9ec7e83c95688bdb9b68d59d77db91eaeab
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-02-07 17:48:47 +0900

    Create Text File

---
+++ app/src/main/java/com/example/bbip_clone/ui/theme/Text.kt
@@ -0,0 +1,13 @@
+package com.example.bbip_clone.ui.theme
+
+const val home = "홈"
+const val allView = "전체보기"
+const val board = "게시판"
+const val weekStudy = "이번 주 스터디"
+const val comingTodos = "다가오는 일정"
+const val certification = "출석 인증"
+const val place = "장소확인"
+const val archive = "아카이브"
+const val progress = "스터디 진척도"
+const val weekActivities = "주차별 활동"
+const val studyMembers = "스터디원"
