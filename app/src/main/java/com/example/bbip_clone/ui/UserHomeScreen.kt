9a59953471d20bd72d47a0e01dc53db8b521fa1b
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-02-07 18:40:08 +0900

    Create AppBar

--- app/src/main/java/com/example/bbip_clone/ui/UserHomeScreen.kt
+++ app/src/main/java/com/example/bbip_clone/ui/UserHomeScreen.kt
@@ -1,25 +1,104 @@
 package com.example.bbip_clone.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.bbip_clone.ui.theme.home
+import com.example.bbip_clone.ui.theme.title3_m20
 
 @Composable
 fun UserHomeScreen(navController: NavController) {
-    Column(
-        Modifier.fillMaxSize()
+    Scaffold(
+        topBar = { AppBar() }
     ) {
-        Text("UserHomeScreen")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            HorizontalDivider()
+
+        }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar() {
+    TopAppBar(
+        modifier = Modifier.height(42.dp),
+        title = {
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = home, style = title3_m20)
+                Spacer(modifier = Modifier.weight(1f))
+
+                Box {
+                    Icon(
+                        imageVector = Icons.Filled.Notifications,
+                        contentDescription = "Notifications",
+                        modifier = Modifier
+                            .size(20.dp)
+                            .clickable {
+                                // 추가 api 설정 부분
+                            }
+                    )
+                    // 알림 여부(빨간 점)
+                    Box(
+                        modifier = Modifier
+                            .size(5.dp)
+                            .background(Color.Red, CircleShape)
+                            .align(Alignment.TopEnd)
+                    )
+                }
+                Spacer(Modifier.width(8.dp))
+
+                Icon(
+                    imageVector = Icons.Filled.Person,
+                    contentDescription = "Profile",
+                    modifier = Modifier
+                        .size(20.dp)
+                        .clickable {
+                            // 추가 api 설정 부분
+                        }
+                )
+                Spacer(Modifier.width(28.dp))
+            }
+        },
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun UserHomePreview(){
+fun UserHomePreview() {
     UserHomeScreen(rememberNavController())
 }
