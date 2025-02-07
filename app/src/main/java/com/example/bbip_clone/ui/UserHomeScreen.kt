36b0541040a1c904f7090a777b2f029c34599ef4
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-02-07 21:18:51 +0900

    Extract IconButton at UserHomeScreen

--- app/src/main/java/com/example/bbip_clone/ui/UserHomeScreen.kt
+++ app/src/main/java/com/example/bbip_clone/ui/UserHomeScreen.kt
@@ -2,6 +2,7 @@ package com.example.bbip_clone.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,17 +16,23 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -35,8 +42,15 @@ import com.example.bbip_clone.ui.theme.title3_m20
 
 @Composable
 fun UserHomeScreen(navController: NavController) {
+    var noticeCheck by remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit) {
+        // 알림 여부(빨간 점)
+        noticeCheck = true
+    }
+
     Scaffold(
-        topBar = { AppBar() }
+        topBar = { AppBar(noticeCheck) }
     ) {
         Column(
             modifier = Modifier
@@ -51,7 +65,10 @@ fun UserHomeScreen(navController: NavController) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AppBar() {
+fun AppBar(noticeCheck: Boolean) {
+    val notificationIcon = Icons.Filled.Notifications
+    val personIcon = Icons.Filled.Person
+
     TopAppBar(
         modifier = Modifier.height(42.dp),
         title = {
@@ -63,37 +80,37 @@ fun AppBar() {
                 Spacer(modifier = Modifier.weight(1f))
 
                 Box {
-                    Icon(
-                        imageVector = Icons.Filled.Notifications,
-                        contentDescription = "Notifications",
-                        modifier = Modifier
-                            .size(20.dp)
-                            .clickable {
-                                // 추가 api 설정 부분
-                            }
-                    )
-                    // 알림 여부(빨간 점)
-                    Box(
-                        modifier = Modifier
-                            .size(5.dp)
-                            .background(Color.Red, CircleShape)
-                            .align(Alignment.TopEnd)
-                    )
+                    IconButton(notificationIcon)
+                    if (noticeCheck)
+                        Box(
+                            modifier = Modifier
+                                .size(4.dp)
+                                .background(Color.Red, CircleShape)
+                                .align(Alignment.TopEnd)
+                        )
                 }
-                Spacer(Modifier.width(8.dp))
+                Spacer(Modifier.width(12.dp))
 
-                Icon(
-                    imageVector = Icons.Filled.Person,
-                    contentDescription = "Profile",
-                    modifier = Modifier
-                        .size(20.dp)
-                        .clickable {
-                            // 추가 api 설정 부분
-                        }
-                )
+                IconButton(personIcon)
                 Spacer(Modifier.width(28.dp))
             }
         },
+    )
+}
+
+@Composable
+fun IconButton(icon: ImageVector) {
+    Icon(
+        imageVector = icon,
+        contentDescription = "$icon",
+        modifier = Modifier
+            .size(20.dp)
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = rememberRipple(bounded = false, radius = 14.dp)
+            ) {
+                // 추가 api 설정 부분
+            }
     )
 }
 
