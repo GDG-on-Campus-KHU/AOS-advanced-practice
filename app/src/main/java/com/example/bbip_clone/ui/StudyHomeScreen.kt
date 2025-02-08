c23f9ca672bab11bc9811eb08de0f8a316240d10
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-02-08 15:40:02 +0900

    Add MaskImage at StudyHome

--- app/src/main/java/com/example/bbip_clone/ui/StudyHomeScreen.kt
+++ app/src/main/java/com/example/bbip_clone/ui/StudyHomeScreen.kt
@@ -1,12 +1,15 @@
 package com.example.bbip_clone.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -17,10 +20,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.bbip_clone.R
 import com.example.bbip_clone.ui.theme.Gray9
 
 @Composable
@@ -40,6 +45,16 @@ fun StudyHomeScreen(navController: NavController) {
                 .fillMaxWidth()
                 .align(Alignment.TopCenter)
         )
+
+        Column(modifier = Modifier.align(Alignment.TopEnd)) {
+            Spacer(Modifier.height(170.dp))
+            Image(
+                painter = painterResource(R.drawable.study_mask),
+                contentDescription = "StudyMask",
+                modifier = Modifier.size(300.dp)
+            )
+        }
+
         Scaffold(
             modifier = Modifier.fillMaxSize(),
             containerColor = Color.Transparent,
