a44a39ab02c3400f290cb7a9105388484cd11b1b
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-02-09 15:47:28 +0900

    Create Icon at theme

---
+++ app/src/main/java/com/example/bbip_clone/ui/theme/Icon.kt
@@ -0,0 +1,14 @@
+package com.example.bbip_clone.ui.theme
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
+
+val notificationsIcon = Icons.Filled.Notifications
+val personIcon = Icons.Filled.Person
+val moreVertIcon = Icons.Filled.MoreVert
+val homeIcon = Icons.Filled.Home
+val dateRangeIcon = Icons.Filled.DateRange
