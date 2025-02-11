f549f06adf8fb679f9654e625055f25877f1fc48
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-02-11 17:29:41 +0900

    Add icon

--- app/src/main/java/com/example/bbip_clone/ui/theme/Icon.kt
+++ app/src/main/java/com/example/bbip_clone/ui/theme/Icon.kt
@@ -2,6 +2,7 @@ package com.example.bbip_clone.ui.theme
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.MoreVert
@@ -13,4 +14,5 @@ val personIcon = Icons.Filled.Person
 val moreVertIcon = Icons.Filled.MoreVert
 val homeIcon = Icons.Filled.Home
 val dateRangeIcon = Icons.Filled.DateRange
-val ArrowRight = Icons.AutoMirrored.Filled.KeyboardArrowRight
+val arrowRightIcon = Icons.AutoMirrored.Filled.KeyboardArrowRight
+val addIcon = Icons.Filled.Add
