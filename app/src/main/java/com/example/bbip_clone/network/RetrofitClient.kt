d9257b11dad42def7a37e658c809d99194d53754
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-02-11 17:29:18 +0900

    Add teamMember data

--- app/src/main/java/com/example/bbip_clone/network/RetrofitClient.kt
+++ app/src/main/java/com/example/bbip_clone/network/RetrofitClient.kt
@@ -29,9 +29,9 @@ fun getStudyWeekData(id: String): List<StudyWeekData> {
     studyDataList.add(StudyWeekData("5", "", "20250103"))
     studyDataList.add(StudyWeekData("6", "", "20250110"))
     studyDataList.add(StudyWeekData("7", "", "20250117"))
-    studyDataList.add(StudyWeekData("8", "", "20250124"))
-    studyDataList.add(StudyWeekData("9", "", "20250215"))
-    studyDataList.add(StudyWeekData("10", "오늘은 공부하지 말까여..", "20250223"))
+    studyDataList.add(StudyWeekData("8", "", "20250215"))
+    studyDataList.add(StudyWeekData("9", "오늘은 공부하지 말까여..", "20250222"))
+    studyDataList.add(StudyWeekData("10", "", "20250230"))
 
     return studyDataList
 }
@@ -39,6 +39,7 @@ fun getStudyWeekData(id: String): List<StudyWeekData> {
 fun getTeamMember(id: String): List<TeamMember> {
     val teamMembers = listOf(
         TeamMember("Andy", "팀장", R.drawable.ic_launcher_background),
+        TeamMember("Andy", "팀원", R.drawable.ic_launcher_background),
         TeamMember("Andy", "팀원", R.drawable.ic_launcher_background),
         TeamMember("Andy", "팀원", R.drawable.ic_launcher_background)
     )
