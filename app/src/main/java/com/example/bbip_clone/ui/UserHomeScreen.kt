c222fc8a0fa355586350c7a59628e7fd0bc4095c
Taehoon Kim <124406982+taeh-kim@users.noreply.github.com>
2025-02-22 11:17:13 +0900

    Add basic NoticeBar

--- app/src/main/java/com/example/bbip_clone/ui/UserHomeScreen.kt
+++ app/src/main/java/com/example/bbip_clone/ui/UserHomeScreen.kt
@@ -1,29 +1,53 @@
 package com.example.bbip_clone.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.bbip_clone.network.getNotice
 import com.example.bbip_clone.network.getNotionCheck
+import com.example.bbip_clone.ui.theme.Gray2
+import com.example.bbip_clone.ui.theme.body1_sb16
+import com.example.bbip_clone.ui.theme.body2_m14
+import com.example.bbip_clone.ui.theme.notice
 
 @Composable
 fun UserHomeScreen(navController: NavController) {
     var noticeCheck by remember { mutableStateOf(false) }
+    var noticeData by remember { mutableStateOf("") }
 
     LaunchedEffect(Unit) {
-        // 알림 여부(빨간 점) api 추가
         noticeCheck = getNotionCheck(true)
+        noticeData = getNotice("id")
+
     }
 
     Scaffold(
@@ -35,7 +59,46 @@ fun UserHomeScreen(navController: NavController) {
                 .padding(it)
         ) {
             HorizontalDivider()
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+                    .padding(vertical = 22.dp)
+                    .background(Gray2, shape = RoundedCornerShape(12.dp))
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier.wrapContentSize(),
+                    contentAlignment = Alignment.TopEnd
+                ) {
+                    Text(
+                        text = notice,
+                        style = body1_sb16,
+                        color = Color.Red,
+                        modifier = Modifier
+                            .padding(horizontal = 8.dp, vertical = 4.dp)
+                    )
 
+                    if (noticeCheck) {
+                        Box(
+                            modifier = Modifier
+                                .size(4.dp)
+                                .background(Color.Red, CircleShape)
+                                .align(Alignment.TopEnd)
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Text(
+                    text = noticeData,
+                    style = body2_m14
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
