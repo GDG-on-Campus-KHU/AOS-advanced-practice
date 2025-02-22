f77d98f9986d9aeb5c1bb5919f39f02aa438c7ff
Taehoon Kim <124406982+taeh-kim@users.noreply.github.com>
2025-02-22 18:59:13 +0900

    Add Text in TimeRing

--- app/src/main/java/com/example/bbip_clone/ui/UserHomeScreen.kt
+++ app/src/main/java/com/example/bbip_clone/ui/UserHomeScreen.kt
@@ -30,6 +30,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,23 +39,30 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.bbip_clone.network.getNotice
 import com.example.bbip_clone.network.getNotionCheck
+import com.example.bbip_clone.network.getStudySummaryData
 import com.example.bbip_clone.ui.theme.Gray2
+import com.example.bbip_clone.ui.theme.Gray5
 import com.example.bbip_clone.ui.theme.Gray6
 import com.example.bbip_clone.ui.theme.Gray8
+import com.example.bbip_clone.ui.theme.MainWhite
 import com.example.bbip_clone.ui.theme.PrimaryDark
 import com.example.bbip_clone.ui.theme.body1_sb16
 import com.example.bbip_clone.ui.theme.body2_m14
+import com.example.bbip_clone.ui.theme.caption2_m12
 import com.example.bbip_clone.ui.theme.notice
+import com.example.bbip_clone.ui.theme.title4_sb24
 
 @Composable
 fun UserHomeScreen(navController: NavController) {
     var noticeCheck by remember { mutableStateOf(false) }
     var noticeText by remember { mutableStateOf("") }
 
+    val studySummaryDataList = remember { getStudySummaryData() }
+    val todayStudy = studySummaryDataList.firstOrNull { it.isToday }
+
     LaunchedEffect(Unit) {
         noticeCheck = getNotionCheck(true)
         noticeText = getNotice("id")
-
     }
 
     Scaffold(
@@ -70,17 +79,72 @@ fun UserHomeScreen(navController: NavController) {
                 noticeCheck = noticeCheck
             )
             Spacer(modifier = Modifier.height(13.dp))
+
             Box(
                 modifier = Modifier.fillMaxWidth(),
                 contentAlignment = Alignment.Center
             ) {
-                TimeRing()
+                TimeRing(
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                todayStudy?.let { study ->
+                    Column(
+                        modifier = Modifier.align(Alignment.Center),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .background(Gray2, shape = RoundedCornerShape(10.dp))
+                                .padding(horizontal = 8.dp, vertical = 4.dp)
+                                .align(Alignment.CenterHorizontally)
+                        ) {
+                            Text(
+                                text = "TODAY",
+                                color = Gray8,
+                                style = caption2_m12,
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Text(
+                            text = study.title,
+                            style = title4_sb24,
+                            color = MainWhite,
+                            textAlign = TextAlign.Center,
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis,
+                            modifier = Modifier.fillMaxWidth(0.55f) // 더 좋은 대안?
+                        )
+
+                        Spacer(modifier = Modifier.height(12.dp))
+
+                        Text(
+                            text = "${study.startTime} - ${study.endTime}",
+                            style = body2_m14,
+                            color = Gray5
+                        )
+
+                        Spacer(modifier = Modifier.height(2.dp))
+
+                        Text(
+                            text = study.location,
+                            style = body2_m14,
+                            color = Gray5
+                        )
+                    }
+                }
             }
         }
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun UserHomePreview() {
     UserHomeScreen(rememberNavController())
 }
+
+
