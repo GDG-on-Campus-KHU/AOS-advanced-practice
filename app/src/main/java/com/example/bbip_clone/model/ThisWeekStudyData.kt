c88eb451f37fb6412d8157c7233d198ee900c730
Taehoon Kim <124406982+taeh-kim@users.noreply.github.com>
2025-02-24 08:06:13 +0900

    Add ThisWeekStudyData data class

---
+++ app/src/main/java/com/example/bbip_clone/model/ThisWeekStudyData.kt
@@ -0,0 +1,12 @@
+package com.example.bbip_clone.model
+
+data class ThisWeekStudyData(
+    val title : String,
+    val round : String,
+    val theme : String,
+    val content : String,
+    val studyDate : String,
+    val studyStartTime : String,
+    val studyEndTime : String,
+    val location : String
+)
